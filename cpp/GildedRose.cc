baf36d337dd0a32cabfd8053bc1d5ddbba826e2a
Lucas Giger <lucas@atfinity.ch>
2018-06-05 14:39:54 +0200

    Refactored updateQuality function

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -2,79 +2,103 @@
 
 GildedRose::GildedRose(vector<Item> & items) : items(items)
 {}
-    
+
 void GildedRose::updateQuality() 
 {
-    for (int i = 0; i < items.size(); i++)
+    for (Item & item : items) {
+        updateItem(item);
+    }
+}
+
+void GildedRose::updateItem(Item & item)
+{
+    updateItemQuality(item);
+    updateItemSellIn(item);
+    updateItemQualityPostSellIn(item);
+}
+
+void GildedRose::updateItemQuality(Item & item)
+{
+    if (item.name == "Aged Brie")
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        incrementQuality(item);
+    }
+    else if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+    {
+        incrementQuality(item);
+        if (item.sellIn < 11)
         {
-            if (items[i].quality > 0)
-            {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
+            incrementQuality(item);
+        }
+
+        if (item.sellIn < 6)
+        {
+            incrementQuality(item);
+        }
+    }
+    else if (item.name == "Sulfuras, Hand of Ragnaros")
+    {
+        return;
+    }
+    else if (item.name == "Conjured Mana Cake")
+    {
+        decrementQuality(item);
+        decrementQuality(item);
+    }
+    else
+    {
+        decrementQuality(item);
+    }
+}
+
+void GildedRose::incrementQuality(Item & item)
+{
+    if (item.quality < 50)
+    {
+        item.quality = item.quality + 1;
+    }
+}
+
+void GildedRose::decrementQuality(Item & item)
+{
+    if (item.quality > 0)
+    {
+        item.quality = item.quality - 1;
+    }
+}
+
+void GildedRose::updateItemSellIn(Item & item)
+{
+    if (item.name != "Sulfuras, Hand of Ragnaros")
+    {
+        item.sellIn = item.sellIn - 1;
+    }
+}
+
+void GildedRose::updateItemQualityPostSellIn(Item & item)
+{
+    if (item.sellIn < 0)
+    {
+        if (item.name == "Aged Brie")
+        {
+            incrementQuality(item);
+        }
+        else if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+        {
+            item.quality = 0;
+        }
+        else if (item.name == "Sulfuras, Hand of Ragnaros")
+        {
+            return;
+        }
+        else if (item.name == "Conjured Mana Cake")
+        {
+            decrementQuality(item);
+            decrementQuality(item);
         }
         else
         {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
-            {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
+            decrementQuality(item);
         }
     }
 }
--- cpp/GildedRose.h
+++ cpp/GildedRose.h
@@ -15,10 +15,18 @@ public:
 
 class GildedRose
 {
+private:
+    void updateItem(Item & item);
+    void updateItemQuality(Item & item);
+    void updateItemSellIn(Item & item);
+    void updateItemQualityPostSellIn(Item & item);
+    void incrementQuality(Item & item);
+    void decrementQuality(Item & item);
+
 public:
     vector<Item> & items;
     GildedRose(vector<Item> & items);
-    
+
     void updateQuality();
 };
 
